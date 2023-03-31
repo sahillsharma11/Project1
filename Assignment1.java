8e27b422aa8dd920dba85a9e50b8eb4486011bd4
Sahil Sharrma <129388506+sahillsharma11@users.noreply.github.com>
2023-03-31 14:31:57 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,21 @@
+class Assignment1{
+    public static void main(String[] args){
+int a=-5;
+if (a>0){
+    System.out.println("Number is positive");
+    
+    
+}
+else {
+    System.out.println("Number is negative");
+    
+}
+
+return;
+
+    }
+
+
+
+
+}
